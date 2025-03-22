1e369168398c7e27b0fa69bcdf5b443cb42c7eb9
kalinduagathisi <kalinduagathisi@gmail.com>
2025-03-22 19:56:27 +0530

    added pods to the project and used CLTypingLable Pod

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import CLTypingLabel // import pod
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "⚡️FlashChat"
+        
+        // code to proceed with normal way, without using pods
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        
+//        for letter in titleText {
+//            print("-")
+//            print(0.1 * charIndex)
+//            print(letter)
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
 
        
     }
