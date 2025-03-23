7ff2ccc74607fb3da8e23eb960016d1410dbb910
kalinduagathisi <kalinduagathisi@gmail.com>
2025-03-23 08:25:07 +0530

    implemented logout functionality and hide back btn in chat view

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -6,20 +6,37 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
+import FirebaseAuth
 import UIKit
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
 
     }
-    
+
     @IBAction func sendPressed(_ sender: UIButton) {
     }
-    
+
+    // on logout button press
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            
+            // go to root view controller, use popToRootView()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
 
 }
