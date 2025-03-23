295a523ec2ef9966889ccc0ec798712781c4e78b
kalinduagathisi <kalinduagathisi@gmail.com>
2025-03-23 07:52:18 +0530

    added user login functionality

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -6,15 +6,33 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
+import FirebaseAuth
+import FirebaseCore
+import FirebaseFirestore
 import UIKit
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
-    
 
     @IBAction func loginPressed(_ sender: UIButton) {
+
+        if let email = emailTextfield.text,
+            let password = passwordTextfield.text
+        {
+
+            Auth.auth().signIn(withEmail: email, password: password) {
+                [weak self] authResult, error in
+
+                if let error = error {
+                    print(error)
+                } else {
+                    self!.performSegue(
+                        withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
     }
-    
+
 }
